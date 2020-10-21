652522a5a726118a46d48b5d14d148bf6d3edf7e
User <User@201-10>
2020-10-21 15:28:45 +0900

    최초 업로드

---
+++ 01_java_basic/src/step1_05/controlStatement/IfEx08.java
@@ -0,0 +1,37 @@
+package step1_05.controlStatement;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * 중첩 if 문
+ * 
+ * */
+
+public class IfEx08 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("#성적입력 : ");
+		int getGrade = scan.nextInt();
+		
+		if ( getGrade >= 60 ) {
+			if ( getGrade == 100 ) {		// 들여쓰기 중요
+				System.out.println("만점");
+			}
+			System.out.println("합격");
+		}
+		
+		if ( getGrade < 60 ) {
+			System.out.println("불합격");
+		}
+		
+		
+		scan.close();
+
+		
+	}
+
+}
