fea896d388b9d1640fdcd23427198e508cc6f1a2
User <User@201-10>
2020-10-22 17:46:24 +0900

    save

---
+++ 05_java_file/src/step5_02/file/FileEx03_문제.java
@@ -0,0 +1,29 @@
+package step5_02.file;
+
+//# 파일 저장하기 : 연습문제1
+
+
+public class FileEx03_문제 {
+
+	public static void main(String[] args) {
+		
+		// 김철수/20,이만수/30,이영희/40
+		
+		String[] names = {"김철수", "이만수", "이영희"};
+		int[]     ages = {	   20,     30,     40};
+		
+		String fileName = "fileTest01.txt";
+		
+		String data = "";
+
+		for (int i = 0; i < names.length; i++) {
+			data += names[i];
+			data += ages[i];
+			if(i != names.length-1) {
+				data += ",";
+			}
+		}
+		
+	}
+
+}
